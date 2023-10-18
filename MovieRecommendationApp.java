a69e46dc930b9a3fd840a9143c201625cf0ce0bc
KevinXJarema <tun48388@temple.edu>
2023-10-17 18:41:07 -0700

    Added some error handling aswell as the ability to exit the application.

--- MovieRecommendationApp.java
+++ MovieRecommendationApp.java
@@ -10,33 +10,46 @@ import java.util.Scanner;
 
 public class MovieRecommendationApp {
     public static void main(String[] args) {
-        // Create a list of movies by scraping data from a website
+        while (true) {
+            // Get user preferences (genre)
+            String userPreference = getUserPreference();
 
+            if ("exit".equalsIgnoreCase(userPreference)) {
+                System.out.println("Exiting the application.");
+                break; // Exit the loop
+            }
 
-        // Get user preferences (genre)
-        String userPreference = getUserPreference();
-
-
-
-        // Recommend movies based on the user's genre preference
-        List<Movie> Movies = scrapeMovieData("https://www.imdb.com/search/title/?genres="+userPreference+"&explore=genres&title_type=movie");
-        for (Movie movie : Movies) {
-            System.out.println("Title: " + movie.getTitle());
-            System.out.println("Rating: " + movie.getRating());
-            System.out.println("Genre: " + movie.getGenre());
-            System.out.println();
+            if (userPreference != null && !userPreference.isEmpty()) {
+                List<Movie> movies = scrapeMovieData("https://www.imdb.com/search/title/?genres=" + userPreference + "&explore=genres&title_type=movie");
+                if (movies != null && !movies.isEmpty()) {
+                    for (Movie movie : movies) {
+                        System.out.println("Title: " + movie.getTitle());
+                        System.out.println("Rating: " + movie.getRating());
+                        System.out.println("Genre: " + movie.getGenre());
+                        System.out.println();
+                    }
+                } else {
+                    System.out.println("No movies found for the specified genre.");
+                }
+            } else {
+                System.out.println("Invalid user preference.");
+            }
         }
     }
 
     private static List<Movie> scrapeMovieData(String url) {
         List<Movie> movies = new ArrayList<>();
+        int maxPages = 1; // Adjust the number of pages to scrape
 
         try {
-            int page = 0;
-            while (page < 1) {
-                Document doc = Jsoup.connect(url).get();
+            for (int page = 1; page <= maxPages; page++) {
+                Document doc = Jsoup.connect(url + "&page=" + page).get();
                 Elements movieElements = doc.select("div.lister-item.mode-advanced");
-                String output;
+
+                if (movieElements.isEmpty()) {
+                    break; // No more movies on subsequent pages
+                }
+
                 for (Element element : movieElements) {
                     String title = element.select("h3 a").text();
                     String rating = element.select("strong").text();
@@ -45,19 +58,17 @@ public class MovieRecommendationApp {
                     Movie movie = new Movie(title, rating, genre);
                     movies.add(movie);
                 }
-            page++;
             }
-
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
+            System.out.println("An error occurred while scraping movie data.");
         }
         return movies;
     }
+
     private static String getUserPreference() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your movie genre preference: ");
+        System.out.print("Enter your movie genre preference (enter 'exit' to quit): ");
         return scanner.nextLine().toLowerCase();
     }
-
-
 }
